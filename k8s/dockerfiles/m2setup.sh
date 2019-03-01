c7204b45f73be1d70d5de86604adbade3e914963
Chris Trahey <christrahey@gmail.com>
2019-02-28 17:20:50 -0800

    got magento working

---
+++ k8s/dockerfiles/m2setup.sh
@@ -0,0 +1,16 @@
+bin/magento setup:install  \
+  --db-host=mysql \
+  --db-name=magento \
+  --db-user=root \
+  --db-password=rootpass \
+  --admin-firstname=Magento \
+  --admin-lastname=User \
+  --admin-email=user@example.com \
+  --admin-user=admin \
+  --admin-password=admin123 \
+  --language=en_US \
+  --currency=USD \
+  --timezone=America/Los_Angeles \
+  --use-rewrites=1
+
+
